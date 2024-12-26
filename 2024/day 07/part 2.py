ad7a20ff2a74d5c42561638e6a56c95490faa6d7
Cyrus Yip <cyrusyip@cyrusyip.com>
2024-12-26 14:22:11 -0800

    add 2024 solutions

---
+++ 2024/day 07/part 2.py
@@ -0,0 +1,28 @@
+import itertools
+
+from utils.api import get_input
+
+inp = get_input(7)
+
+total = 0
+for line in inp.splitlines():
+    split = line.split(": ")
+    value = int(split[0])
+    numbers = [int(x) for x in split[1].split(" ")]
+    for operations in itertools.product(["+", "*", "|"], repeat=len(numbers) - 1):
+        calc = 0
+        for i, number in enumerate(numbers):
+            if i == 0:
+                calc = number
+                continue
+            if operations[i - 1] == "+":
+                calc += number
+            elif operations[i - 1] == "*":
+                calc *= number
+            else:
+                calc = int(f"{calc}{number}")
+        if calc == value:
+            total += value
+            break
+
+print(total)
